811b5f7447836882e31f2d20f09b1272c2e0f0a8
NokMyo <82665094+NokMyo@users.noreply.github.com>
2026-09-14 13:28:48 +0900

    build: link WinHTTP and DPAPI for account licensing

--- build.bat
+++ build.bat
@@ -10,5 +10,5 @@ popd
 if not "%engine_build_status%"=="0" exit /b 1
 clang --target=x86_64-pc-windows-msvc -c src\fortuna.s -o build\fortuna.obj || exit /b 1
 rc /nologo /c65001 /fo build\app.res resources\app.rc || exit /b 1
-link /nologo /nodefaultlib /machine:x64 /subsystem:windows,6.02 /entry:WinMainCRTStartup /dynamicbase /highentropyva /nxcompat /Brepro /map:build\fortuna.map /out:build\FebiusFortuna.exe build\fortuna.obj build\app.res engine\build\FortunaOracleStatic.lib user32.lib kernel32.lib gdi32.lib comdlg32.lib shell32.lib comctl32.lib advapi32.lib || exit /b 1
+link /nologo /nodefaultlib /machine:x64 /subsystem:windows,6.02 /entry:WinMainCRTStartup /dynamicbase /highentropyva /nxcompat /Brepro /map:build\fortuna.map /out:build\FebiusFortuna.exe build\fortuna.obj build\app.res engine\build\FortunaOracleStatic.lib user32.lib kernel32.lib gdi32.lib comdlg32.lib shell32.lib comctl32.lib advapi32.lib winhttp.lib crypt32.lib || exit /b 1
 exit /b 0
